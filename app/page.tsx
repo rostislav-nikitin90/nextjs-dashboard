50ab25186600b5343da087adcc1a3929cdf56726
rostislav-nikitin90 <rosnikitin90@gmail.com>
2026-05-20 19:49:49 +0200

    Completed W03 Assignment: Next.js Application

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,7 @@ export default function Page() {
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
+            loading="eager"
           />
           <Image
             src="/hero-mobile.png"
